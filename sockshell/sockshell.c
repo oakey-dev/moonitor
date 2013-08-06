3d5b4004841978c835c576c59738f1a2902a4617
Peter Brantsch <peter.brantsch@kabelmail.de>
2013-08-06 14:01:49 +0200

    catch SIGPIPE
    Now catching SIGPIPE to avoid program termination when e.g. a client disconnects.

--- sockshell/sockshell.c
+++ sockshell/sockshell.c
@@ -7,7 +7,6 @@
 
 /* It is ESSENTIAL to bind to localhost as no authentication and encryption is implemented yet */
 #define ADDRESS "127.0.0.1"
-#define ADDRESS6 "::1"
 #define PORT 60000
 #define BUFSIZE 128
 #define BACKLOG 1024 /* maximum queue length for the listening socket */
@@ -20,6 +19,12 @@ void* start_sockshell(void* p_arg){
 	pthread_t last_session;
 	socklen_t socksize = sizeof(struct sockaddr_in);
 
+	/* make sure SIGPIPE doesn't result in termination */
+	struct sigaction sig_act;
+	memset(&sig_act,0,sizeof(struct sigaction));
+	sig_act.sa_handler = catch_sigpipe;
+	sigaction(SIGPIPE,&sig_act,NULL);
+
 	struct sockaddr_in addr;
 	memset(&addr,0,sizeof(addr)); /* zero out the struct */
 	addr.sin_family = AF_INET;	
@@ -30,9 +35,19 @@ void* start_sockshell(void* p_arg){
 	bind(sock,(struct sockaddr*)&addr,sizeof(addr));
 	listen(sock,BACKLOG);
 
+	int status;
 	for(;;){ /* listen for incoming connections, open a session for each one */
 		consock = accept(sock,(struct sockaddr*)&addr,&socksize);
-		pthread_create(&last_session,NULL,session,(void*)consock);
+		if(consock<0){
+			perror("error accepting connections on socket");
+		}
+#ifndef NDEBUG
+		printf("sockshell main thread (thread id:%ld) accepted connection\n",(long)pthread_self());
+#endif
+		status = pthread_create(&last_session,NULL,session,(void*)consock);
+		if(status != 0){
+			pthread_error(status,"error creating thread for sockshell session");
+		}
 	}
 
 	return 0;
@@ -48,7 +63,15 @@ void* session(void* p_sock){
 	snprintf(buf,BUFSIZE,"Welcome to moonitor shell. Session thread id: %ld\n",(long)pthread_self());
 	nwrite(consock,buf,strlen(buf));
 
-	close(consock);
+	int status=0;
+	while(status>=0){
+		status = nwrite(consock,ps,strlen(ps));
+		read(consock,buf,BUFSIZE);
+	}
+
+	if(close(consock) != 0){
+		perror("error closing socket");
+	}
 	return 0;
 }
 
@@ -67,4 +90,8 @@ int nwrite(int fd, const void* buf,size_t count){
 		}
 	}
 	return written;
+}
+
+void catch_sigpipe(int i){
+	printf("caught SIGPIPE\n");
 }
--- sockshell/sockshell.h
+++ sockshell/sockshell.h
@@ -12,9 +12,14 @@
 #include <pthread.h>
 #include <errno.h>
 #include <stdio.h>
+#include <signal.h>
+
+#define pthread_error(error_number,message)\
+	do{ errno =  error_number; perror(message); } while(0)
 
 void* start_sockshell(void*);
 void* session(void*);
 int nwrite(int,const void*,size_t);
+void catch_sigpipe(int);
 
 #endif /* SOCKSHELLH */
