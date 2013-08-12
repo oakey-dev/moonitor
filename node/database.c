8ca82917c4daf79daf452121bef8e2c958b37522
Peter Brantsch <peter.brantsch@kabelmail.de>
2013-08-12 23:35:17 +0200

    start implementing commands/db access in listener

--- node/database.c
+++ node/database.c
@@ -28,3 +28,4 @@ int sql_exec(char* sql) {
    }
    return 0;
 }
+
--- node/database.h
+++ node/database.h
@@ -4,7 +4,7 @@
 int sql_open();
 void sql_close();
 
-// vor first version / tests
+// for first version / tests
 int sql_exec(char* sql);
 
 // TODO: replace sql_exec with:
